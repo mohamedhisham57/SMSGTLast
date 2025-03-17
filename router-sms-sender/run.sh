45a1bd1622ca02de7516dbf583da24f851a0373d
mohamedhisham57 <99441359+mohamedhisham57@users.noreply.github.com>
2025-03-17 11:00:33 +0200

    Update run.sh

--- router-sms-sender/run.sh
+++ router-sms-sender/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
-
 CONFIG_PATH=/data/options.json
+
+echo "Contents of $CONFIG_PATH:"
+cat $CONFIG_PATH
 
 # Create config file from Home Assistant options
 jq '{
@@ -10,6 +12,9 @@ jq '{
   "password": .router_password,
   "default_recipient": .default_recipient
 }' $CONFIG_PATH > /app/config.json
+
+echo "Generated config.json:"
+cat /app/config.json
 
 # Start the addon service
 cd /app
